5a7f143a32b986c694e14a75cb07347175c3822c
Davi Felix <davifelix@usp.br>
2023-04-16 19:07:51 -0300

    getters e setters da classe combate

--- Combate.java
+++ Combate.java
@@ -1,9 +1,18 @@
 public class Combate {
-  String player1;
-  String player2;
+  private String player1;
+  private String player2;
 
   public Combate(String player1, String player2) {
     this.player1 = player1;
     this.player2 = player2;
   }
+
+  public String get_player1() {
+    return player1;
+  }
+
+  public String get_player2() {
+    return player2;
+  }
+
 }
