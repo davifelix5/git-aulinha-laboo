d4f97d37fd1335d050e08d854822d2860b014fd6
Luiz <you@example.com>
2023-04-16 19:19:10 -0300

    adicionar pokemons

--- Pokemon.java
+++ Pokemon.java
@@ -1,3 +1,10 @@
 class Pokemon {
   // Classe de um pokemon
+  private String pokemon1;
+  private String pokemon2;
+
+  public Pokemon(String pokemon1, String pokemon2) {
+    this.pokemon1 = pokemon1;
+    this.pokemon2 = pokemon2;
+  }
 }
